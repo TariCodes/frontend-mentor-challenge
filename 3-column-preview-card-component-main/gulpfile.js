af31430251056053980a0e7ae4824f46b1ef62ad
TariCodes <godblessika@gmail.com>
2024-05-04 02:46:20 +0100

    added and modified the gulpfile, index and less files

--- 3-column-preview-card-component-main/gulpfile.js
+++ 3-column-preview-card-component-main/gulpfile.js
@@ -1,4 +1,18 @@
-const gulp = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const less = require('gulp-less');
 const cssnano = require('gulp-cssnano');
+
+
+function styles() {
+    return src('./src/*.less')
+        .pipe(less())
+        .pipe(cssnano())
+        .pipe(dest('public/dist/styles'))
+}
+
+function watchTask() {
+    watch(['./src/*.less'], series(styles))
+}
+
+exports.default = series(styles, watchTask);
 
